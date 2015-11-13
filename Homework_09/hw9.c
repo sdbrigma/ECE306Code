7d9b51a9d71fb3b755fe9bb1ed1c9af4cc56ed22
Steffon Brigman <steffon.brigman@gmail.com>
2015-11-13 15:54:02 -0500

    Current progress on HW9

--- Homework_09/hw9.c
+++ Homework_09/hw9.c
@@ -7,9 +7,106 @@ void getResistorMenu(unsigned int adc_value){
   adc_value = adc_value >> 6; // shift 10 bit value by 6 bits
   switch(adc_value){
   case MENU1:
-      display_2 = "Black";
-      posL2 = LINE_POS_L3;
-      display_4 = "0";
-      posL4 = LINE_POS_L5;
+    display_1 = "Color";
+    posL1 = LINE_POS_L3;
+    display_3 = "Value";
+    posL3 = LINE_POS_L3;
+    display_2 = "Black";
+    posL2 = LINE_POS_L3;
+    display_4 = "0";
+    posL4 = LINE_POS_L5;
+    ADC_Process();
+  case MENU2:
+    display_1 = "Color";
+    posL1 = LINE_POS_L3;
+    display_3 = "Value";
+    posL3 = LINE_POS_L3;
+    display_2 = "Brown";
+    posL2 = LINE_POS_L3;
+    display_4 = "1";
+    posL4 = LINE_POS_L5;
+    ADC_Process();
+  case MENU3:
+    display_1 = "Color";
+    posL1 = LINE_POS_L3;
+    display_3 = "Value";
+    posL3 = LINE_POS_L3;
+    display_2 = "Red";
+    posL2 = LINE_POS_L4;
+    display_4 = "2";
+    posL4 = LINE_POS_L5;
+    ADC_Process();
+  case MENU4:
+    display_1 = "Color";
+    posL1 = LINE_POS_L3;
+    display_3 = "Value";
+    posL3 = LINE_POS_L3;
+    display_2 = "Orange";
+    posL2 = LINE_POS_L2;
+    display_4 = "3";
+    posL4 = LINE_POS_L5;
+    ADC_Process();
+  case MENU5:
+    display_1 = "Color";
+    posL1 = LINE_POS_L3;
+    display_3 = "Value";
+    posL3 = LINE_POS_L3;
+    display_2 = "Yellow";
+    posL2 = LINE_POS_L2;
+    display_4 = "4";
+    posL4 = LINE_POS_L5;
+    ADC_Process();
+  case MENU6:
+    display_1 = "Color";
+    posL1 = LINE_POS_L3;
+    display_3 = "Value";
+    posL3 = LINE_POS_L3;
+    display_2 = "Green";
+    posL2 = LINE_POS_L3;
+    display_4 = "5";
+    posL4 = LINE_POS_L5;
+    ADC_Process();
+  case MENU7:
+    display_1 = "Color";
+    posL1 = LINE_POS_L3;
+    display_3 = "Value";
+    posL3 = LINE_POS_L3;
+    display_2 = "Blue";
+    posL2 = LINE_POS_L3;
+    display_4 = "6";
+    posL4 = LINE_POS_L5;
+    ADC_Process();
+  case MENU8:
+    display_1 = "Color";
+    posL1 = LINE_POS_L3;
+    display_3 = "Value";
+    posL3 = LINE_POS_L3;
+    display_2 = "Violet";
+    posL2 = LINE_POS_L2;
+    display_4 = "7";
+    posL4 = LINE_POS_L5;
+    ADC_Process();
+  case MENU9:
+    display_1 = "Color";
+    posL1 = LINE_POS_L3;
+    display_3 = "Value";
+    posL3 = LINE_POS_L3;
+    display_2 = "Gray";
+    posL2 = LINE_POS_L3;
+    display_4 = "8";
+    posL4 = LINE_POS_L5;
+    ADC_Process();
+  case MENU10:
+    display_1 = "Color";
+    posL1 = LINE_POS_L3;
+    display_3 = "Value";
+    posL3 = LINE_POS_L3;
+    display_2 = "White";
+    posL2 = LINE_POS_L3;
+    display_4 = "9";
+    posL4 = LINE_POS_L5;
+    ADC_Process();
+    
+  default: break;
   }
 }
--- Homework_09/switch.c
+++ Homework_09/switch.c
@@ -24,30 +24,22 @@ void Switches_Process(void){
 //******************************************************************************
   if (!(P4IN & SW1)){ 
     ADC_Process();
-    display_1 = "          ";
-    posL1 = ZERO;
-    display_2 = "          ";
-    posL2 = ZERO;
-    display_3 = "          ";
-    posL3 = ZERO;
-    display_4 = "          ";
-    posL4 = ZERO;
     while(ALWAYS){
       if (!(P4IN & SW2)){break;}
       ADC_Process(); // read ADC value for thumbwheel
       if(ADC_Thumb <= 340){// Divides range of ADC values into three for main menu
-        display_1 = "Resistors"; 
-        posL1 = ZERO;
-        //Display_Process();
+        display_1 = "Resistors";
+        posL1 = LINE_POS_L0;
+        display_2 = "          ";
+        posL2 = LINE_POS_L0;
+        display_3 = "          ";
+        posL3 = LINE_POS_L0;
+        display_4 = "          ";
+        posL4 = LINE_POS_L0;
         ADC_Process();
         
         // RESISTOR CODES
-        if(!(P4IN & SW1)){
-          ClrDisplay();
-          display_1 = "Color";
-          posL1 = 3;
-          display_3 = "Value";
-          posL3 = 3;
+        if(!(P4IN & SW2)){
           while(ALWAYS){
             if (!(P4IN & SW1)){break;}
             ADC_Process();
@@ -58,15 +50,25 @@ void Switches_Process(void){
       
       else if(ADC_Thumb >= 681){ // this range the last third of the full range of values for the ADC
         display_1 = "Song";
-        posL1 = 3;  
-        //Display_Process();
+        posL1 = LINE_POS_L3;
+        display_2 = "          ";
+        posL2 = LINE_POS_L0;
+        display_3 = "          ";
+        posL3 = LINE_POS_L0;
+        display_4 = "          ";
+        posL4 = LINE_POS_L0;
         ADC_Process();
       }
       
       else{ // If it's not in the lower or upper third it's in the middle
         display_1 = "Shapes";
-        posL1 = 2;
-        //Display_Process();
+        posL1 = LINE_POS_L2;
+        display_2 = "          ";
+        posL2 = LINE_POS_L0;
+        display_3 = "          ";
+        posL3 = LINE_POS_L0;
+        display_4 = "          ";
+        posL4 = LINE_POS_L0;
         ADC_Process();
       }
     }
